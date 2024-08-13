c846d3555b860f4aff4c66bd51f8368cf73d1040
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-08-13 18:49:22 +0900

    [D2] Title: 간단한 369게임, Time: 103 ms, Memory: 44,792 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,9 +1,9 @@
 N = int(input())
-
+ 
 for i in range(1, N+1):
     cnt = 0
     for j in str(i):
         if j in ['3', '6', '9']:
             cnt += 1
-
+ 
     print('-'*cnt if cnt > 0 else i, end = ' ')
