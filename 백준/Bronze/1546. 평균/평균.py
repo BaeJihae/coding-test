6701b2778df796bde7cb4e4d6c5a796b955e934f
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-08-30 00:55:25 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,13 +1,6 @@
-i = input
-n = int(i())
+N = int(input())
+lst = list(map(int, input().split()))
 
-score = list(map(int, i().split()))
-
-m = max(score)
-
-sum = 0
-
-for i in score:
-    sum += ( i / m ) * 100
-
-print(sum/n)
+M = max(lst)
+score_sum = sum(score / M * 100 for score in lst)
+print(score_sum/N)
