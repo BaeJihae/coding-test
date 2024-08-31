9c88a630915c0a4193c108f2d66d8f0087adcab0
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-09-01 02:24:58 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 145 ms, Memory: 45,060 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,31 +1,24 @@
-def inorder(c_n):
+def inorder(current_n):
     global answer
-
-    if c_n == 0:
-        return
-
-    inorder(left_node[c_n])
-    answer.append(word_dict[c_n])
-    inorder(right_node[c_n])
+    if current_n:
+        inorder(left_node[current_n])
+        answer += word_dict[current_n]
+        inorder(right_node[current_n])
 
 
 for tc in range(1, 11):
     N = int(input())
-
-    word_dict = [0] * (N + 1)
-    left_node = [0] * (N + 1)
-    right_node = [0] * (N + 1)
+    word_dict, left_node, right_node = [[0] * (N + 1) for _ in range(3)]
 
     for _ in range(N):
         lst = list(input().split())
         word_dict[int(lst[0])] = lst[1]
 
-        if len(lst) == 3:
+        if len(lst) >= 3:
             left_node[int(lst[0])] = int(lst[2])
-        elif len(lst) == 4:
-            left_node[int(lst[0])] = int(lst[2])
+        if len(lst) >= 4:
             right_node[int(lst[0])] = int(lst[3])
 
-    answer = []
+    answer = ''
     inorder(1)
-    print(f'#{tc}', ''.join(answer))
+    print(f'#{tc}', answer)
