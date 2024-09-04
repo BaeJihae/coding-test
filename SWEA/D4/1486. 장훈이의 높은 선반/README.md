15f4d8d281d69ec40d167af5f50e65bd71ddb41e
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-09-04 14:06:49 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 568 ms, Memory: 55,316 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/README.md
@@ -0,0 +1,15 @@
+# [D4] 장훈이의 높은 선반 - 1486 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b7Yf6ABcBBASw) 
+
+### 성능 요약
+
+메모리: 55,316 KB, 시간: 568 ms, 코드길이: 484 Bytes
+
+### 제출 일자
+
+2024-09-04 14:06
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,23 @@
+
+
+def sub_set(bits):
+    global answer
+
+    if len(bits) == N:
+        sum = 0
+        for i in range(N):
+            if bits[i] == 1:
+                sum += lst[i]
+        if sum >= B and answer > sum:
+            answer = sum
+        return
+
+    sub_set(bits + [0])
+    sub_set(bits + [1])
+
+for tc in range(1, int(input()) + 1):
+    N, B = map(int, input().split())
+    lst = list(map(int, input().split()))
+    answer = float('inf')
+    sub_set([])
+    print(f'#{tc} {answer - B}')
