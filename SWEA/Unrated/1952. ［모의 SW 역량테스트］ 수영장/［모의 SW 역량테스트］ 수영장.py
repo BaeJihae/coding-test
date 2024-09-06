3f9670c60414650603ff1607e5b7791d50ed1106
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-09-06 14:05:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 697 ms, Memory: 71,652 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,7 +1,7 @@
 # 0: 1일, 1: 한 달, 2: 세 달
 def dfs(fee, idx, visited):
     global answer
-    
+
     # 기저조건 -> 12달을 체크 후의 요금 최저값 계산
     if idx >= 12:
         answer = min(fee, answer)
@@ -10,31 +10,31 @@ def dfs(fee, idx, visited):
     # 가지치기 -> 현재의 요금이 최저값보다 크다면 종료
     if fee > answer:
         return
-    
+
     for k in range(3):
         # 요금 계산하지 않은 월
         if not visited[idx]:
             # 세 달 요금 계산
             if k == 2:
-                
+
                 for i in range(idx, idx + 3):
                     if i < 12:
                         visited[idx] = True
-                
+
                 dfs(fee + three_month_fee, idx + 3, visited)
-                
+
                 for i in range(idx, idx + 3):
                     if i < 12:
                         visited[idx] = False
             else:
-                
-                visited[idx] = True 
-                
+
+                visited[idx] = True
+
                 if k == 0:      # 1일 요금 계산
                     dfs(fee + lst[idx] * day_fee, idx + 1, visited)
                 elif k == 1:    # 한 달 요금 계산
                     dfs(fee + month_fee, idx + 1, visited)
-                    
+
                 visited[idx] = False
         # 요금 계산을 했거나 할 필요가 없는 월
         else:
@@ -44,7 +44,7 @@ for tc in range(1, int(input()) + 1):
     # 입력
     day_fee, month_fee, three_month_fee, year_fee = map(int, input().split())
     lst = list(map(int, input().split()))
-    
+
     # 최저 수영장 요금
     answer = year_fee
     # 방문 노드
