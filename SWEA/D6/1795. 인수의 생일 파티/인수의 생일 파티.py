1bc359777c2f10e9748d252fafe5755dee00ea08
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-09-12 14:44:28 +0900

    [D6] Title: 인수의 생일 파티, Time: 348 ms, Memory: 55,932 KB -BaekjoonHub

--- SWEA/D6/1795. 인수의 생일 파티/인수의 생일 파티.py
+++ SWEA/D6/1795. 인수의 생일 파티/인수의 생일 파티.py
@@ -2,16 +2,13 @@ from heapq import heappop, heappush
 
 
 # adj : 인접리스트, n : 노드의 개수
-def dijkstra(adj, adj_op, n, s):
+def dijkstra(adj, n, s):
     # s -> x까지 노드의 최소 시간
     distance_lst = [float('inf')] * (n + 1)
-    # x -> s까지 노드의 최소 시간
-    distance_op_lst = [float('inf')] * (n + 1)
     # 시작 노드와 끝 노드 초기화
-    distance_op_lst[s], distance_lst[s] = 0, 0
+    distance_lst[s] = 0
 
     min_heap = [(0, s)]  # (노드의 최소 거리, 시작 노드)
-    min_heap_op = [(0, s)]  # (반대로 갈 때의 노드의 최소 거리, 시작 노드)
 
     # 시작 노드부터 각 노드까지의 최소 시간 구하기
     while min_heap:
@@ -20,16 +17,8 @@ def dijkstra(adj, adj_op, n, s):
             if distance_lst[nxt_n] > d + nxt_d:
                 distance_lst[nxt_n] = d + nxt_d
                 heappush(min_heap, (distance_lst[nxt_n], nxt_n))
-    
-    # 각 노드에서 시작 노드까지의 최소 시간 구하기
-    while min_heap_op:
-        d, n = heappop(min_heap_op)
-        for nxt_n, nxt_d in adj_op[n]:
-            if distance_op_lst[nxt_n] > d + nxt_d:
-                distance_op_lst[nxt_n] = d + nxt_d
-                heappush(min_heap_op, (distance_op_lst[nxt_n], nxt_n))
 
-    return distance_lst, distance_op_lst
+    return distance_lst
 
 
 def solution():
@@ -40,14 +29,14 @@ def solution():
         # 인접 리스트 초기화
         adj = [[] * (n + 1) for _ in range(n + 1)]
         adj_op = [[] * (n + 1) for _ in range(n + 1)]
-        
+
         for _ in range(m):
             # x : 출발 노드, y : 도착 노드, c : 거리
             x, y, c = map(int, input().strip().split())
             adj[x].append((y, c))     # 정상 방향으로의 인접 리스트 할당
             adj_op[y].append((x, c))  # 반대 방향의 인접 리스트 할당
 
-        result_adj, result_adj_op = dijkstra(adj, adj_op, n, s)
+        result_adj, result_adj_op = dijkstra(adj, n, s), dijkstra(adj_op, n, s)
 
         # 오고 가는데 걸리는 시간의 최댓값 구하기
         answer = 0
