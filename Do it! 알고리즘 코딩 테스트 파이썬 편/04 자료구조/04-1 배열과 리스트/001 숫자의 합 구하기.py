fa448e93fcbcc036dccb89491a15668838f3a446
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-08 21:35:59 +0900

    study: [Doit] Ch04-1 문제 1 구현

---
+++ Do it! 알고리즘 코딩 테스트 파이썬 편/04 자료구조/04-1 배열과 리스트/001 숫자의 합 구하기.py
@@ -0,0 +1,15 @@
+# N개의 숫자가 공백 없이 쓰여 있다. 이 숫자를 모두 합해 출력하는 프로그램을 작성하시오.
+
+import sys
+
+input = sys.stdin.readline
+write = sys.stdout.write
+
+n = int(input()) # 숫자의 개수
+numbers = list(input()) # 공백 없이 주어진 N개의 숫자
+
+answer = 0 # 숫자의 합
+for i in range(0, n):
+    answer += int(numbers[i])
+
+write(str(answer))
