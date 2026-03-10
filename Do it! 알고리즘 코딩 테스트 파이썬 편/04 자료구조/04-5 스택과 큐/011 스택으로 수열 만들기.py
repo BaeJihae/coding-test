fb56a620111860f72e97bab45de58ffd002dfebe
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-10 17:40:42 +0900

    study: [Doit] Ch4-5 문제 11 구현

---
+++ Do it! 알고리즘 코딩 테스트 파이썬 편/04 자료구조/04-5 스택과 큐/011 스택으로 수열 만들기.py
@@ -0,0 +1,57 @@
+# 스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다.
+# 스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+
+# 1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다.
+# 이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다.
+# 이를 계산하는 프로그램을 작성하라.
+
+import sys
+from collections import deque
+
+readline = sys.stdin.readline
+write = sys.stdout.write
+
+n = int(readline()) # n
+stack = []  # 수열을 만들기 위한 stack
+answer = [] # +, -를 저장하기 위한 리스트
+
+# i = 1 # 증가할 수
+# for _ in range(n):
+#     a = int(readline()) # 비교할 대상
+    
+#     while(i <= n + 1):
+#         if stack and stack[-1] == a:
+#             stack.pop()
+#             answer.append('-')
+#             break
+    
+#         stack.append(i)
+#         answer.append('+')
+#         i += 1
+
+# if stack:
+#     write("NO")
+# else:
+#     for ans in answer:
+#         write(ans + '\n')
+
+# 코드 최적화
+i = 1 # 증가할 수
+for _ in range(n):
+    a = int(readline()) # 비교할 대상
+    
+    while(a >= i):
+        stack.append(i)
+        answer.append('+')
+        i += 1
+    
+    if a != stack.pop():
+        break
+    
+    answer.append('-')
+
+if stack:
+    write("NO")
+else:
+    for ans in answer:
+        write(ans + '\n')
