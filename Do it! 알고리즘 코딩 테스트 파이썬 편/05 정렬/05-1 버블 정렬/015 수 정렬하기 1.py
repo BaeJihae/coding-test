463ced4d0cb690a0f413e6f9e0890ffcffaee2cb
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-11 15:52:40 +0900

    study: [Doit] Ch05-1 문제 15 구현

---
+++ Do it! 알고리즘 코딩 테스트 파이썬 편/05 정렬/05-1 버블 정렬/015 수 정렬하기 1.py
@@ -0,0 +1,27 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+import sys
+
+readline = sys.stdin.readline
+write = sys.stdout.write
+
+N = int(readline()) # n
+
+L = list()
+for _ in range(N):
+    L.append(int(readline()))
+
+# 버블 정렬
+# for i in range(N, 0, -1):
+#     count = 0
+#     for j in range(0, i - 1):
+#         if L[j] > L[j+1]:
+#             L[j], L[j+1] = L[j+1], L[j]
+#             count += 1
+#     if count == 0:
+#         break
+
+L.sort()
+
+for l in L:
+    write(str(l) + '\n')    
