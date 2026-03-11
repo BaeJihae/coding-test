1d7d44ff6734fe279d8253fb830ca7406fea5787
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-11 14:43:17 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -9,15 +9,7 @@ L = list()
 for _ in range(N):
     L.append(int(readline()))
 
-# 버블 정렬
-for i in range(N, 0, -1):
-    count = 0
-    for j in range(0, i - 1):
-        if L[j] > L[j+1]:
-            L[j], L[j+1] = L[j+1], L[j]
-            count += 1
-    if count == 0:
-        break
+L.sort()
 
 for l in L:
-    write(str(l) + '\n')
+    write(str(l) + '\n')    
