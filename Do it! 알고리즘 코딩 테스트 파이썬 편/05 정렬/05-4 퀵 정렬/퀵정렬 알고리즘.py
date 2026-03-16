8c54c576f938c225a323bd30239fc5d26df2fa5a
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-16 19:23:55 +0900

    study: 퀵 정렬 알고리즘 구현

--- Do it! 알고리즘 코딩 테스트 파이썬 편/05 정렬/05-4 퀵 정렬/퀵정렬 알고리즘.py
+++ Do it! 알고리즘 코딩 테스트 파이썬 편/05 정렬/05-4 퀵 정렬/퀵정렬 알고리즘.py
@@ -1,3 +1,4 @@
+# 내가 구현한 퀵 정렬
 from collections import deque
 
 lst = [4, 2, 6, 3, 9, 1, 8, 10, 13, 41]
@@ -34,12 +35,48 @@ def quick_sort(s, e):
     for i in range(s, e + 1):
         lst[i] = tmp.popleft()
     
-    print(f's: {s}, e: {e}, lst: {lst}')
-    
     quick_sort(s, k - 1)
     quick_sort(k + 1, e)
     
     return
+
+quick_sort(0, len(lst) - 1)
+print(lst)
+
+# 문제점1. deque를 사용해서 appendleft 할 때 순서가 뒤집힘.
+# 문제점2. 매번 deque를 생성하여 퀵 정렬의 장점인 리스트 내에서 swap 형태가 이루어 지지 않음.
+
+
+# 해결한 코드
+lst = [4, 2, 6, 3, 9, 1, 8, 10, 13, 41]
+
+def quick_sort(s, e):
+    global lst
+
+    if s >= e:
+        return
+
+    # 중간 pivot 선택
+    m = s + (e - s) // 2
+    pivot = lst[m]
+
+    # pivot을 맨 앞으로 이동
+    lst[s], lst[m] = lst[m], lst[s]
+
+    k = s  # pivot 위치
+
+    # partition
+    for i in range(s + 1, e + 1):
+        if lst[i] < pivot:
+            k += 1
+            lst[i], lst[k] = lst[k], lst[i]
+
+    # pivot을 제자리로 이동
+    lst[s], lst[k] = lst[k], lst[s]
+
+    quick_sort(s, k - 1)
+    quick_sort(k + 1, e)
+
 
 quick_sort(0, len(lst) - 1)
 print(lst)
