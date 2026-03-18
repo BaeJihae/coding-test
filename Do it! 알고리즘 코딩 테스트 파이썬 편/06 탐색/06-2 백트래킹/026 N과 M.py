a4a16c6237595386dce564165fc24f60df482e17
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2026-03-18 16:27:58 +0900

    study: [Doit] Ch06-2 문제 026 구현

---
+++ Do it! 알고리즘 코딩 테스트 파이썬 편/06 탐색/06-2 백트래킹/026 N과 M.py
@@ -0,0 +1,26 @@
+import sys
+
+readline = sys.stdin.readline
+write = sys.stdout.write
+
+N, M = map(int, readline().split())
+
+visited = [0] * (N + 1)
+
+def back(n, depth):
+    if depth == M:
+        for i in str(n):
+            write(i + " ")
+        write("\n")
+        return
+    
+    for i in range(1, N + 1):
+        if not visited[i]:
+            visited[i] = 1
+            back(n*10 + i, depth + 1)
+            visited[i] = 0
+
+for i in range(1, N + 1):
+    visited[i] = 1
+    back(i, 1)
+    visited[i] = 0
